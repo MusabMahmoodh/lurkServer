85dddd03e13e96b6d879e81e9fae020580b8d678
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-02-17 21:37:44 +0530

    added first phase of rest API

--- base/views.py
+++ base/views.py
@@ -3,27 +3,27 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
+from .serializers import ProductSerializer
 # Create your views here.
 from .products import products
 
 
 @api_view(['GET'])
 def getRoutes(request):
-    products = Product.objects.all()
-    serielizer = ProductSerializer(products, many=True)
-    return Response(serielizer.data)
+    return Response("Hi")
 
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
+    products = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(products, many=False)
+    return Response(serializer.data)
